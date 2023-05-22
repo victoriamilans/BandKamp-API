b4c34dc1dcc60cecbed63b7692ebb0990785df07
Victoria <victoria-milan@outlook.com>
2023-05-22 12:36:48 -0300

    feat/ create users serializer

---
+++ users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password", "first_name", "last_name", "is_superuser"]
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_superuser(**validated_data)
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        instance.set_password(validated_data["password"])
+        instance.save()
+
+        return instance
