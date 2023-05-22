95e0656106ac36884d199a7f0004e49445d8a330
Victoria <victoria-milan@outlook.com>
2023-05-22 12:36:03 -0300

    feat/ create users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True, error_messages={"unique": "This field must be unique."})
+    username = models.CharField(max_length=150, unique=True, error_messages={"unique": "A user with that username already exists."})
