992139e9730ccb997bda1c452eb77f4d0b3b342a
Victoria <victoria-milan@outlook.com>
2023-05-22 12:38:42 -0300

    feat/ create song model

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    class Meta:
+        ordering = ("id",)
+
+    title = models.CharField(max_length=255)
+    duration = models.CharField(max_length=255)
+
+    album = models.ForeignKey(
+        "albums.Album",
+        on_delete=models.CASCADE,
+        related_name="songs",
+    )
